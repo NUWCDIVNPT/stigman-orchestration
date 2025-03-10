9ea7b3500779dd3a256fec5c42d81ff88fdf32db
cd-rite <61710958+cd-rite@users.noreply.github.com>
2025-03-10 16:33:20 -0400

    removed keycloak revalidation config

--- certs/dod/p7b-convert.sh
+++ certs/dod/p7b-convert.sh
@@ -7,7 +7,5 @@ file_in=$1
 
 openssl pkcs7 -print_certs -in $file_in -out DoD_Root_CAs.pem
 
-# Then create PKCS12 file, prompting for password
-# docker-compose specifies KC_SPI_TRUSTSTORE_FILE_PASSWORD=password
-openssl pkcs12 -export -nokeys  -out DoD_Root_CAs.p12 -in  DoD_Root_CAs.pem
+
 
